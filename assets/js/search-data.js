5e71f1a150befdf7eb0d22153f1d1ce07c233fa0
gzliyan113 <gzliyan113@gatech.edu>
2026-06-04 23:06:06 +0100

    Deploy responsive profile update

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -66,13 +66,6 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=wLfoeakAAAAJ", "_blank");
         },
       },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
